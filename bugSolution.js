ac353d8d0e5c6dfe1f94ac0b293754d30e3613a3
BugHunter-X <askzenai@gmail.com>
2025-02-05 08:22:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+function foo(x) {
+  if (x === null) {
+    return 0; // Handle null case
+  } else if (x === undefined) {
+    return 1; // Handle undefined case
+  } else if (isNaN(x)){
+    return 'Not a Number'; //Handle NaN case
+  } else {
+    return x + 1; // Normal case
+  }
+}
+
+console.log(foo(null)); // Output: 0
+console.log(foo(undefined)); // Output: 1
+console.log(foo(5)); // Output: 6
+console.log(foo(NaN)); // Output: Not a Number
